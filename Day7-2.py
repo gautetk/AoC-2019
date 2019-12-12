c4f75ad01cfa380068d166908aaf5b49f9e2deb1
Kolstad <Gaute.Kolstad@dnvgl.com>
2019-12-12 22:04:55 +0100

    WIP Day7-2

---
+++ Day7-2.py
@@ -0,0 +1,100 @@
+import os
+
+
+def main():
+    chalange = 'Day7'
+    sourcePath = '..\\Source\\'
+    source = sourcePath + chalange + '.txt'
+
+    with open(source, 'r') as sourceFile:
+        inputCode = sourceFile.read()
+
+    splitedInputCode = inputCode.split(',')
+    initialMemory = map(int, splitedInputCode)
+
+
+    part2(initialMemory)
+
+
+def part2(initialMemory):
+    initialSettings = [5, 6, 7, 8, 9]
+
+    maxSignal = 0
+    for a in initialSettings:
+        for b in filter(lambda b: b not in [a], initialSettings):
+            for c in filter(lambda c: c not in [a, b], initialSettings):
+                for d in filter(lambda d: d not in [a, b, c], initialSettings):
+                    for e in filter(lambda e: e not in [a, b, c, d], initialSettings):
+                        aSettings = [a, b, c, d, e]
+                        signal = 0
+                        ip = [0]*5
+                        # outputs = [None]*5
+                        memory = [initialMemory[:]]*5
+                        for j in range(100):
+                            for i, setting in enumerate(aSettings):
+                                signal, memory[i] = runCode(memory[i], [setting, signal], ip[i])
+                        if signal > maxSignal:
+                            maxSignal = signal
+    print maxSignal
+
+
+def runCode(memory, inputValues, ip):
+    i = 0
+    count = 0
+    output = []
+    while count < 100000000:
+        count += 1
+        istruction = memory[ip]
+        if istruction % 100 == 1:
+            memory[memory[ip + 3]] = getValue(memory, istruction,ip, 1) + getValue(memory, istruction, ip, 2)
+            ip += 4
+        elif istruction % 100 == 2:
+            memory[memory[ip + 3]] = getValue(memory, istruction,ip, 1) * getValue(memory, istruction, ip, 2)
+            ip += 4
+        elif istruction % 100 == 3:
+            memory[memory[ip + 1]] = inputValues[i]
+            if i == 1:
+                i = i+1
+            ip += 2
+        elif istruction % 100 == 4:
+            value = getValue(memory, istruction, ip, 1)
+            output.append(value)
+            ip += 2
+        elif istruction % 100 == 5:
+            if getValue(memory, istruction, ip, 1) != 0:
+                ip = getValue(memory, istruction, ip, 2)
+            else:
+                ip += 3
+        elif istruction % 100 == 6:
+            if getValue(memory, istruction, ip, 1) == 0:
+                ip = getValue(memory, istruction, ip, 2)
+            else:
+                ip += 3
+        elif istruction % 100 == 7:
+            if getValue(memory, istruction, ip, 1) < getValue(memory, istruction, ip, 2):
+                memory[memory[ip + 3]] = 1
+            else:
+                memory[memory[ip + 3]] = 0
+            ip += 4
+        elif istruction % 100 == 8:
+            if getValue(memory, istruction, ip, 1) == getValue(memory, istruction, ip, 2):
+                memory[memory[ip + 3]] = 1
+            else:
+                memory[memory[ip + 3]] = 0
+            ip += 4
+        elif istruction % 100 == 99:
+            break
+    return output[0], memory[:]
+
+
+def getValue(memory, instruction, ip, valuePos):
+    mode = (instruction/(10*10**valuePos)) % 10
+    if mode == 0:
+        value = memory[memory[ip+valuePos]]
+    else:
+        value = memory[ip+valuePos]
+    return value
+
+
+if __name__ == "__main__":
+    main()
